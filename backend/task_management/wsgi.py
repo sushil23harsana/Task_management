2b76633c54197b6d5be498ffdf4d46848b8e6070
Sushil Harsana <106249517+sushil23harsana@users.noreply.github.com>
2025-06-25 17:01:59 +0530

    Update wsgi.py

--- backend/task_management/wsgi.py
+++ backend/task_management/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_management.settings')
 
 application = get_wsgi_application()
+
